1c0e0e8051c61ffce96e19080d4e7fe46e2c140e
Kristi <123495259+Kahelm621@users.noreply.github.com>
2024-04-12 12:48:09 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// JavaScript code for interactivity
+
+// Function to change background color
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightblue";
+  }
+  
+  // Event listener for button click
+  document.getElementById("changeColorBtn").addEventListener("click", changeBackgroundColor);
+  
